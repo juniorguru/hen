502fdc8d6c69e53e0775f541f85bad722e1e508b
Honza Javorek <mail@honzajavorek.cz>
2024-04-08 12:33:48 +0200

    run main

--- jg/hen/cli.py
+++ jg/hen/cli.py
@@ -6,7 +6,9 @@ import click
 logger = logging.getLogger("jg.hen")
 
 
-@click.group()
-@click.option("-d", "--debug", default=False, is_flag=True)
-def main(debug: bool = False):
-    logger.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+@click.command()
+@click.argument("profile_url")
+@click.option("-d", "--debug", default=False, is_flag=True, help="Show debug logs")
+def main(profile_url: str, debug: bool):
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+    logger.info(f"URL: {profile_url}")

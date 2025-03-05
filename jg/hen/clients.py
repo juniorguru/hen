e7938884ea66381ff0c5482c00efcc913642ff0f
Honza Javorek <mail@honzajavorek.cz>
2025-03-05 15:42:47 +0100

    provide default instance only if not already provided in args

--- jg/hen/clients.py
+++ jg/hen/clients.py
@@ -11,9 +11,11 @@ USER_AGENT = "JuniorGuruBot (+https://junior.guru)"
 def with_github(fn: Callable[..., Coroutine]) -> Callable[..., Coroutine]:
     @wraps(fn)
     async def wrapper(*args, **kwargs) -> Coroutine:
-        github_api_key = kwargs.pop("github_api_key", None)
-        async with GitHub(github_api_key, user_agent=USER_AGENT) as github:
-            return await fn(*args, github=github, **kwargs)
+        if github_api_key := kwargs.pop("github_api_key", None):
+            async with GitHub(github_api_key, user_agent=USER_AGENT) as github:
+                return await fn(*args, github=github, **kwargs)
+        else:
+            return await fn(*args, **kwargs)
 
     return wrapper
 
@@ -21,9 +23,12 @@ def with_github(fn: Callable[..., Coroutine]) -> Callable[..., Coroutine]:
 def with_http(fn: Callable[..., Coroutine]) -> Callable[..., Coroutine]:
     @wraps(fn)
     async def wrapper(*args, **kwargs) -> Coroutine:
-        async with httpx.AsyncClient(
-            follow_redirects=True, headers={"User-Agent": USER_AGENT}
-        ) as client:
-            return await fn(*args, http=client, **kwargs)
+        if kwargs.get("http", None):
+            return await fn(*args, **kwargs)
+        else:
+            async with httpx.AsyncClient(
+                follow_redirects=True, headers={"User-Agent": USER_AGENT}
+            ) as client:
+                return await fn(*args, http=client, **kwargs)
 
     return wrapper
